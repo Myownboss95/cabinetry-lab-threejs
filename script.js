7d3fd87418f64359edad8b2f2e2e6dabfe34e31a
Xpressglobe <tobeodiachi@yahoo.com>
2023-06-25 21:28:49 +0100

    test

--- script.js
+++ script.js
@@ -67,8 +67,8 @@ const onWindowResize = (model, length) => {
          console.log(window.innerWidth)
     // Rest of the code inside onWindowResize function
     const sizeMappings = [
-      { maxSize: 0, desiredSize: 1 },
-        { maxSize: 576, desiredSize: 1 },    // Extra Small (XS)
+      { maxSize: 0, desiredSize: 1.5 },
+        { maxSize: 576, desiredSize: 1.5 },    // Extra Small (XS)
       { maxSize: 768, desiredSize: 1 },    // Small (SM)
       { maxSize: 992, desiredSize: 4 },    // Medium (MD)
       { maxSize: 1200, desiredSize: 5 },  // Large (LG)
